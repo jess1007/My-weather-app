37dbec3024a8f1d90919637b9b61ca58c93432ed
jess1007 <jessica.gernelle@gmail.com>
2022-03-14 18:06:50 +1100

    Weather skeleton with search engine displaying current temperature Celsius + current time in current location.

---
+++ src/index.js
@@ -0,0 +1,50 @@
+let now = new Date();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let date = `${day} ${hours}:${minutes}`;
+let todayDate = document.querySelector("#date");
+todayDate.innerHTML = `${date}`;
+
+function search(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#searchCity");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${cityInput.value}`;
+  let apiKey = "639a25b57aa39c61186426161599dec9";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=metric`;
+  axios.get(`${apiUrl}`).then(showWeather);
+}
+let form = document.querySelector("form");
+form.addEventListener("submit", search);
+form.addEventListener("click", search);
+
+function showWeather(response) {
+  let currentTemp = Math.round(response.data.main.temp);
+  let todayTemp = document.querySelector("#today-temperature");
+  todayTemp.innerHTML = `${currentTemp}`;
+  let currentWind = Math.round(response.data.wind.speed);
+  let todayWind = document.querySelector("#today-wind");
+  todayWind.innerHTML = `${currentWind}`;
+  let currentHumidity = response.data.main.humidity;
+  let todayHumidity = document.querySelector("#today-humidity");
+  todayHumidity.innerHTML = `${currentHumidity}`;
+  let currentFeelsLike = Math.round(response.data.main.feels_like);
+  let todayFeelsLike = document.querySelector("#today-feels-like");
+  todayFeelsLike.innerHTML = `${currentFeelsLike}`;
+}
+
+function showPosition(position) {
+  console.log(position);
+}
+
+navigator.geolocation.getCurrentPosition(showPosition);
